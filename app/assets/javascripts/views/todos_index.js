f53c572ed5610551adb485d156c7f1207247c97d
rishi-amura <rishi@amuratech.com>
2016-03-07 18:44:00 +0530

    Complete `Clear Completed` Link

--- app/assets/javascripts/views/todos_index.js
+++ app/assets/javascripts/views/todos_index.js
@@ -1,7 +1,8 @@
 RailsBackboneTodo.Views.TodosIndex = Backbone.View.extend({
   template: JST['todos/index'],
   events: {
-    'keyup #todo-input': 'getInput'
+    'keyup #todo-input': 'getInput',
+    'click #clear-completed-link': 'clearCompleted'
   },
   initialize: function(){
     this.collection.on('reset', this.render, this);
@@ -9,6 +10,9 @@ RailsBackboneTodo.Views.TodosIndex = Backbone.View.extend({
     this.collection.on('add', this.updateItemsCounter, this);
     this.collection.on('change', this.updateItemsCounter, this);
     this.collection.on('remove', this.updateItemsCounter, this);
+    this.collection.on('add', this.enableClearLink, this);
+    this.collection.on('change', this.enableClearLink, this);
+    this.collection.on('remove', this.enableClearLink, this);
   },
   render: function(){
     $(this.el).html(this.template());
@@ -30,5 +34,12 @@ RailsBackboneTodo.Views.TodosIndex = Backbone.View.extend({
   },
   updateItemsCounter: function() {
     $('#todo-done-counter').html(this.collection.where({done: false}).length);
+  },
+  enableClearLink: function() {
+    $('#clear-completed-link').attr('hidden', this.collection.where({done: true}).length <= 0);
+  },
+  clearCompleted: function() {
+    var todos_completed = this.collection.where({done: true});
+    _.each(todos_completed, function(model) { model.destroy(); });
   }
 });
